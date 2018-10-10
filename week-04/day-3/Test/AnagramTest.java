26112802ba39cede9fb35112c884f06809ce847b
Tomas <tmoruszewicz@gmail.com>
2018-10-10 15:58:38 +0200

    Anagram tested and completed

--- week-04/day-3/Test/AnagramTest.java
+++ week-04/day-3/Test/AnagramTest.java
@@ -3,13 +3,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class AnagramTest {
-    String correctValue = "Applesefsefse";
-    Apples apple = new Apples();
+
 
 
     @Test
     public void legit() {
-        String returnedValue = apple.getApple();
-        assertEquals("Apple is not equal to returnedValue check that",returnedValue, correctValue);
+        Anagram pupa= new Anagram();
+
+        assertEquals(false, pupa.sortString("kajak","kajak"));
     }
 }
--- week-04/day-3/src/Anagram.java
+++ week-04/day-3/src/Anagram.java
@@ -1,7 +1,21 @@
-import java.util.List;
+import java.util.Arrays;
 
 public class Anagram {
-    String anagram;
-    String word2;
-    List<String> anagramChar =
+
+    public static boolean sortString(String anagram, String word2){
+
+            // convert input string to char array
+            char anagramArray[] = anagram.toCharArray();
+            char word2Array[] = word2.toCharArray();
+
+            Arrays.sort(anagramArray);
+            Arrays.sort(word2Array);
+
+        if (Arrays.equals(word2Array,anagramArray)) return true;
+        else return false;
+        }
+
+    public static void main(String[] args) {
+        System.out.println(sortString("dupsko","okspud"));
+    }
 }
