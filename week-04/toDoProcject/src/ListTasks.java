9351c14361b00a87bff37cbe0b65f866b8a6b4ad
Tomas <tmoruszewicz@gmail.com>
2018-10-11 16:59:31 +0200

    added add and remove with file reading and writing

--- week-04/toDoProcject/src/ListTasks.java
+++ week-04/toDoProcject/src/ListTasks.java
@@ -1,5 +1,50 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class ListTasks {
-    List<String>
+    List<String> storage = new ArrayList<String>();
+    //Do I really need this array where is it used?
+
+    public void addToList(String task){
+        storage.add(task);
+    } //do I really need this method if I read the file? I guess not.
+    public void writeToFile(){
+
+        List<String> storage1 = new ArrayList();
+        try {
+
+            Path filePath = Paths.get("Storage");
+
+            Files.write(filePath, storage1);
+
+        } catch (Exception ex) {
+            System.out.println("Uh-oh, could not write the file!");
+        }
+        //storage1.add("First line of my file");
+    }
+    public void readFromStorage(){
+       Path path = Paths.get("Storage");
+        try{
+            List<String> storage1 = Files.readAllLines(path);
+            System.out.println(storage1);
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+    }
+    public void completed(){
+
+    }
+    public void remove(int index){
+        storage.remove(index);
+    }
+
 }
+
--- week-04/toDoProcject/src/StoriesToDo.java
+++ week-04/toDoProcject/src/StoriesToDo.java
@@ -1,10 +1,45 @@
+import java.util.Arrays;
+
 public class StoriesToDo {
     public static void main(String[] args) {
-        PrintUsage intro = new PrintUsage();
-        System.out.println(intro.header);
-        System.out.println(intro.separator);
-        intro.commandsPrint();
-        //Test of args print:
-        System.out.println(args.toString());
+        if(args.length == 0) {
+            //Printing intro if there are no args
+            PrintUsage intro = new PrintUsage();
+            System.out.println(intro.header);
+            System.out.println(intro.separator);
+            intro.commandsPrint();
+
+            //Test of args print:
+            System.out.println(args.toString());
+        }
+        else if (args[0].equals("-a")){
+            ListTasks task = new ListTasks();
+            for (int i = 0; i <args.length ; i++) {
+                task.addToList(args[i+1].concat(args[i+1]+" "));
+            }
+            task.writeToFile();
+            //remove all arguments from args method
+            Arrays.fill(args,null);
+        }
+        else if (args[0] =="-l"){
+            ListTasks task = new ListTasks();
+            task.readFromStorage();
+            //remove from args method
+            Arrays.fill(args,null);
+        }
+        else if (args[0] == "-r"){
+            ListTasks task = new ListTasks();
+
+            //method for removing indexes from task.content array from the file Storage
+            for (int i = 0; i <args.length ; i++) {
+
+            }
+        }
+        else if (args[0] == "-c"){
+            ListTasks task = new ListTasks();
+            //task.removeTask();
+            //remove from args method
+            Arrays.fill(args,null);
+        }
     }
 }
