fbb037587b001b00c43d0e7842e49a471fd2bc82
Tomas <tmoruszewicz@gmail.com>
2018-11-14 22:04:17 +0100

    Java music class created duuuude

--- week-07/week-08/day-2/ToDoDatabase/src/main/java/ToDoApp/Sounds.java
+++ week-07/week-08/day-2/ToDoDatabase/src/main/java/ToDoApp/Sounds.java
@@ -1,4 +1,32 @@
 package ToDoApp;
 
-public class Sounds {
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+//should I have it as service? or Component?
+public class Sounds implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        playsound();
+    }
+    public static void playsound(){
+        AudioPlayer MGP = AudioPlayer.player;
+        AudioStream BGM;
+        AudioData MD;
+        ContinuousAudioDataStream loop = null;
+        try{
+            //Where should be the file?
+            BGM = new AudioStream(new FileInputStream(("Kid_Laugh.wav")));
+        }catch (IOException E){
+            System.out.println("wrong file location duuuude");
+        }
+        MGP.start(loop);
+
+    }
 }
